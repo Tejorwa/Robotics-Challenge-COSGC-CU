6f7195b9667738e85b028b5f7c7d9db0206f7257
RS-Coop <simpson.r.cooper@gmail.com>
2019-03-17 14:08:32 -0600

    Working on the DoF class, need to test.

--- code/9DoF.py
+++ code/9DoF.py
@@ -1,13 +1,12 @@
 from smbus2 import SMBusWrapper
+from node_modules import DoF
 import time
 
 if __name__=="__main__":
-    add = 0x1E
-    reg = 0x2A
+    D = DoF.DoF()
+
+    D.magRead()
 
     while True:
-        with SMBusWrapper(1) as bus:
-            data = bus.read_i2c_block_data(add,reg,2)
-            print("Y heading Low: "+data)
-
+        print(D.getHeading())
         time.sleep(1)
